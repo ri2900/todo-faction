968ced62000a27410b0748ca78d8e99d55b4e052
ri2900 <ridff29@gmail.com>
2025-05-30 16:16:03 +0600

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+
+// Replace with your Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyBDd2R8FefFOqSe2muB6Zk1jzZ_QhUzNKI",
+  authDomain: "todo-faction.firebaseapp.com",
+  projectId: "todo-faction",
+  storageBucket: "todo-faction.appspot.com",
+  messagingSenderId: "342141679101",
+  appId: "1:342141679101:web:8f5197955351aa68939594"
+};
+firebase.initializeApp(firebaseConfig);
+    
